68f045f0e8a13809a1292ee652b05af397b5f981
Manuel Kloppenburg <mklopp@outlook.de>
2018-07-05 19:14:15 +0200

    Adapt test

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -2,12 +2,12 @@ import os
 import sys
 # I need to add the path to PYTHONPATH so the files can be found
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from src.cryptoexamples.example_string_encryption_password_based import string_encryption_password_based
-from src.cryptoexamples.example_string_encryption_key_based import string_encryption_key_based
-from src.cryptoexamples.example_file_encryption_password_based import file_encryption_password_based
+from src.cryptoexamples.example_symmetric_string_encryption_password_based import string_encryption_password_based
+from src.cryptoexamples.example_symmetric_string_encryption_key_based import string_encryption_key_based
+from src.cryptoexamples.example_symmetric_file_encryption_password_based import file_encryption_password_based
 from src.cryptoexamples.example_hash import string_hash
 from src.cryptoexamples.example_asymmetric_string_encryption import asymmetric_string_encryption
-from src.cryptoexamples.example_signature_rsa import signature_rsa
+from src.cryptoexamples.example_string_signature_rsa import signature_rsa
 
 
 def test_string_encryption_password_based(caplog):
